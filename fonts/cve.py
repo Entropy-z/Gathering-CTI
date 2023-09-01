67071f60d3c691cda2fe6700436543b3b5da1b75
entropy <entropy@Entropy.Entropy>
2023-09-01 04:48:29 -0400

    first

---
+++ fonts/cve.py
@@ -0,0 +1,9 @@
+#NIST
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&search_type=all&isCpeNameSearch=false"
+USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1788.0  uacq"
+
+def ():
+    
